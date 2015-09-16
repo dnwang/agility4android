a8d7c1ad91cf59fdf459a0f992dcf307c505527b
Panda <512570056@qq.com>
2015-09-16 21:45:09 +0800

    repleace CycleGallery to CircularView

---
+++ agility/src/main/src_agility/org/pinwheel/agility/view/SweetCircularView.java
@@ -0,0 +1,537 @@
+package org.pinwheel.agility.view;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.database.DataSetObserver;
+import android.graphics.PointF;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+
+/**
+ * Copyright (C), 2015 <br>
+ * <br>
+ * All rights reserved <br>
+ * <br>
+ *
+ * @author dnwang
+ */
+public class SweetCircularView extends ViewGroup {
+
+    private static final int MOVE_SLOP = 10;
+    private static final long DURATION = 200l;
+
+    protected float sensibility = 0.5f;
+    protected AdapterDataSetObserver dataSetObserver;
+    protected BaseAdapter adapter;
+    protected ArrayList<ItemWrapper> items = new ArrayList<ItemWrapper>();
+    protected int orientation = LinearLayout.HORIZONTAL;
+    protected int currentItemIndex = 0;
+
+    public SweetCircularView(Context context) {
+        super(context);
+        init();
+    }
+
+    public SweetCircularView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public SweetCircularView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init();
+    }
+
+    private void init() {
+        setRecycleItemSize(3);// set default size
+    }
+
+    public void setSensibility(float sensibility) {
+        this.sensibility = Math.max(0, Math.min(1.0f, sensibility));
+    }
+
+    public float getSensibility() {
+        return this.sensibility;
+    }
+
+    public void setOrientation(int orientation) {
+        this.orientation = orientation == LinearLayout.VERTICAL ? orientation : LinearLayout.HORIZONTAL;
+        requestLayout();
+    }
+
+    public int getCurrentIndex() {
+        if (items.size() == 0) {
+            return 0;
+        }
+        return items.get(currentItemIndex).getDataIndex();
+    }
+
+    public void setCurrentIndex(int dataIndex) {
+        if (adapter == null || items.size() == 0) {
+            // show something
+            return;
+        }
+        dataIndex = cycleDataIndex(dataIndex);
+        currentItemIndex = 0;
+        items.get(currentItemIndex).setDataIndex(dataIndex);
+        items.get(currentItemIndex).setStatus(ItemWrapper.FORCE);
+        noNeedLayout = false;
+        requestLayout();
+    }
+
+    public void setAdapter(BaseAdapter cycleAdapter) {
+        if (cycleAdapter == null) {
+            return;
+        }
+        adapter = cycleAdapter;
+        if (dataSetObserver != null) {
+            adapter.unregisterDataSetObserver(dataSetObserver);
+        }
+        dataSetObserver = new AdapterDataSetObserver();
+        adapter.registerDataSetObserver(dataSetObserver);
+    }
+
+    /**
+     * @param size 3,5,7,9 ...
+     */
+    public void setRecycleItemSize(int size) {
+        if (size < 3 || size % 2 == 0) {
+            throw new IllegalStateException("setRecycleItemSize(): the size should be more than 3 of the odd number (3,5,7,9 ...) !");
+        }
+        noNeedLayout = true;
+        resetAllItem();
+        items.clear();
+        for (int itemIndex = 0; itemIndex < size; itemIndex++) {
+            ItemWrapper item = new ItemWrapper();
+            if (adapter != null && adapter.getCount() > 0) {
+                int dataIndex = cycleDataIndex(itemIndex);
+                View convertView = adapter.getView(cycleDataIndex(item.getDataIndex()), null, this);
+                addView(convertView);
+
+                item.setDataIndex(dataIndex);
+                item.setView(convertView);
+            }
+            items.add(item);
+        }
+        noNeedLayout = false;
+        setCurrentIndex(0);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int sizeOfChild = getChildCount();
+        for (int i = 0; i < sizeOfChild; i++) {
+            getChildAt(i).measure(getMeasuredWidth(), getMeasuredHeight());
+        }
+    }
+
+    private boolean noNeedLayout = false;
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        if (noNeedLayout) {
+            return;
+        }
+
+        int sizeOfItem = items.size();
+        if (sizeOfItem == 0) {
+            return;
+        }
+        int sizeOfSideItem = sizeOfItem / 2; // 3,5,7,9 ...
+        int currentDataIndex = items.get(currentItemIndex).getDataIndex();
+
+        int itemWidth = r - l;
+        int itemHeight = b - t;
+        int middleItemLeft = 0;
+        int middleItemTop = 0;
+        int middleItemRight = itemWidth;
+        int middleItemBottom = itemHeight;
+
+        // reset offset
+        for (ItemWrapper item : items) {
+            if (item.getView() != null) {
+                if (orientation == LinearLayout.HORIZONTAL) {
+                    item.getView().setTranslationX(0);
+                } else if (orientation == LinearLayout.VERTICAL) {
+                    item.getView().setTranslationY(0);
+                }
+            }
+        }
+
+        int offset;
+        //middle align
+        ItemWrapper middleItem = items.get(currentItemIndex);
+        recycleItem(middleItem, cycleDataIndex(currentDataIndex));
+        if (middleItem.getView() != null) {
+            middleItem.getView().layout(middleItemLeft, middleItemTop, middleItemRight, middleItemBottom);
+        }
+        // left/top align
+        int sizeOfLeft = currentItemIndex - sizeOfSideItem;
+        offset = 0;
+        for (int leftIndex = currentItemIndex - 1; leftIndex >= sizeOfLeft; leftIndex--) {
+            offset--;
+            ItemWrapper item = items.get(cycleItemIndex(leftIndex));
+            recycleItem(item, cycleDataIndex(currentDataIndex + offset));
+
+            if (item.getView() != null) {
+                if (orientation == LinearLayout.HORIZONTAL) {
+                    // left
+                    int left = middleItemLeft + offset * itemWidth;
+                    item.getView().layout(left, middleItemTop, left + itemWidth, middleItemBottom);
+                } else if (orientation == LinearLayout.VERTICAL) {
+                    // top
+                    int top = middleItemTop + offset * itemHeight;
+                    item.getView().layout(middleItemLeft, top, middleItemRight, top + itemHeight);
+                }
+            }
+        }
+        // right/bottom align
+        int sizeOfRight = currentItemIndex + sizeOfSideItem;
+        offset = 0;
+        for (int rightIndex = currentItemIndex + 1; rightIndex <= sizeOfRight; rightIndex++) {
+            offset++;
+            ItemWrapper item = items.get(cycleItemIndex(rightIndex));
+            recycleItem(item, cycleDataIndex(currentDataIndex + offset));
+
+            if (item.getView() != null) {
+                if (orientation == LinearLayout.HORIZONTAL) {
+                    // right
+                    int right = middleItemRight + offset * itemWidth;
+                    item.getView().layout(right - itemWidth, middleItemTop, right, middleItemBottom);
+                } else if (orientation == LinearLayout.VERTICAL) {
+                    // bottom
+                    int bottom = middleItemBottom + offset * itemHeight;
+                    item.getView().layout(middleItemLeft, bottom - itemHeight, middleItemRight, bottom);
+                }
+            }
+        }
+    }
+
+    protected final void recycleItem(ItemWrapper item, int targetDataIndex) {
+        if (adapter == null || adapter.getCount() < 1) {
+            return;
+        }
+        int itemStatus = item.getStatus();
+        if (itemStatus != ItemWrapper.FORCE) {
+            if (item.getDataIndex() == targetDataIndex && itemStatus == ItemWrapper.USEING) {
+                return;
+            }
+        }
+        // refresh item
+        noNeedLayout = true;
+        item.setDataIndex(targetDataIndex);
+        View convertView = adapter.getView(item.getDataIndex(), item.getView(), this);
+        if (convertView.getParent() != this) {
+            item.setView(null);// release and remove self in group
+            addView(convertView);
+            convertView.measure(getMeasuredWidth(), getMeasuredHeight());// the new view must be measure first
+        }
+        item.setView(convertView);
+        noNeedLayout = false;
+    }
+
+    private PointF lastPoint = new PointF();
+    private boolean isMoving = false;
+    private boolean needIntercept = false;
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                needIntercept = false;
+                lastPoint.set(event.getX(), event.getY());
+                return super.dispatchTouchEvent(event);
+            case MotionEvent.ACTION_MOVE:
+                float absXDiff = Math.abs(event.getX() - lastPoint.x);
+                float absYDiff = Math.abs(event.getY() - lastPoint.y);
+                if (orientation == LinearLayout.HORIZONTAL && absXDiff > absYDiff && absXDiff > MOVE_SLOP) {
+                    needIntercept = true;
+                } else if (orientation == LinearLayout.VERTICAL && absYDiff > absXDiff && absYDiff > MOVE_SLOP) {
+                    needIntercept = true;
+                }
+//                lastPoint.set(event.getX(), event.getY());// must be remove this
+                return super.dispatchTouchEvent(event);
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_CANCEL:
+                return super.dispatchTouchEvent(event);
+            default:
+                return super.dispatchTouchEvent(event);
+        }
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                return super.onInterceptTouchEvent(event);
+            case MotionEvent.ACTION_MOVE:
+                return needIntercept;
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_CANCEL:
+                return super.onInterceptTouchEvent(event);
+            default:
+                return super.onInterceptTouchEvent(event);
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                lastPoint.set(event.getX(), event.getY());
+                break;
+            case MotionEvent.ACTION_MOVE:
+                float xDiff = event.getX() - lastPoint.x;
+                float yDiff = event.getY() - lastPoint.y;
+                float absXDiff = Math.abs(xDiff);
+                float absYDiff = Math.abs(yDiff);
+                if (orientation == LinearLayout.HORIZONTAL && absXDiff > absYDiff) {
+                    // x
+                    moveX(xDiff);
+                } else if (orientation == LinearLayout.VERTICAL && absYDiff > absXDiff) {
+                    // y
+                    moveY(yDiff);
+                }
+                lastPoint.set(event.getX(), event.getY());
+                break;
+            case MotionEvent.ACTION_CANCEL:
+            case MotionEvent.ACTION_UP:
+                // touch release
+                if (isMoving) {
+                    if (items.size() == 0) {
+                        isMoving = false;
+                        break;
+                    }
+                    View v = items.get(currentItemIndex).getView();
+                    if (v == null) {
+                        break;
+                    }
+                    int changeIndex = 0;
+                    if (orientation == LinearLayout.HORIZONTAL) {
+                        // x
+                        float currentOffsetX = v.getTranslationX();
+                        int itemWidth = v.getWidth();
+                        if (currentOffsetX < -itemWidth * sensibility) {
+                            changeIndex = 1;
+                        } else if (currentOffsetX > itemWidth * sensibility) {
+                            changeIndex = -1;
+                        }
+                        if (changeIndex == 0) {
+                            autoMoveX(-currentOffsetX, 0);
+                        } else {
+                            autoMoveX((itemWidth - Math.abs(currentOffsetX)) * -changeIndex, changeIndex);
+                        }
+                    } else if (orientation == LinearLayout.VERTICAL) {
+                        // y
+                        float currentOffsetY = v.getTranslationY();
+                        int itemHeight = v.getHeight();
+                        if (currentOffsetY < -itemHeight * sensibility) {
+                            changeIndex = 1;
+                        } else if (currentOffsetY > itemHeight * sensibility) {
+                            changeIndex = -1;
+                        }
+                        if (changeIndex == 0) {
+                            autoMoveY(-currentOffsetY, 0);
+                        } else {
+                            autoMoveY((itemHeight - Math.abs(currentOffsetY)) * -changeIndex, changeIndex);
+                        }
+                    }
+                }
+                break;
+            default:
+                return super.onTouchEvent(event);
+        }
+        return true;
+    }
+
+    protected final void moveX(float dx) {
+        isMoving = true;
+        for (ItemWrapper item : items) {
+            View view = item.getView();
+            if (view != null) {
+                view.setTranslationX(view.getTranslationX() + dx);
+            }
+        }
+    }
+
+    protected final void moveY(float dy) {
+        isMoving = true;
+        for (ItemWrapper item : items) {
+            View view = item.getView();
+            if (view != null) {
+                view.setTranslationY(view.getTranslationY() + dy);
+            }
+        }
+    }
+
+    protected final void autoMoveX(float dx, final int changeIndex) {
+        ValueAnimator animator = ValueAnimator.ofFloat(0, dx);
+        animator.setDuration(DURATION);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            private float lastValue;
+
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float currentValue = (Float) animation.getAnimatedValue();
+                moveX(currentValue - lastValue);
+                lastValue = currentValue;
+            }
+        });
+        animator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                isMoving = false;
+                currentItemIndex = cycleItemIndex(currentItemIndex + changeIndex);
+                requestLayout();
+            }
+        });
+        animator.start();
+    }
+
+    protected final void autoMoveY(float dy, final int changeIndex) {
+        ValueAnimator animator = ValueAnimator.ofFloat(0, dy);
+        animator.setDuration(DURATION);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            private float lastValue;
+
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float currentValue = (Float) animation.getAnimatedValue();
+                moveY(currentValue - lastValue);
+                lastValue = currentValue;
+            }
+        });
+        animator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                isMoving = false;
+                currentItemIndex = cycleItemIndex(currentItemIndex + changeIndex);
+                requestLayout();
+            }
+        });
+        animator.start();
+    }
+
+    protected final int cycleDataIndex(int dataIndex) {
+        if (adapter == null) {
+            return 0;
+        }
+        int count = adapter.getCount();
+        if (count < 2) {
+            return 0;
+        }
+        if (dataIndex > count - 1) {
+            dataIndex = dataIndex % count;
+        } else if (dataIndex < 0) {
+            dataIndex = count + dataIndex % count;
+        }
+        return dataIndex;
+    }
+
+    protected final int cycleItemIndex(int itemIndex) {
+        int count = items.size();
+        if (count < 2) {
+            return 0;
+        }
+        if (itemIndex > count - 1) {
+            itemIndex = itemIndex % count;
+        } else if (itemIndex < 0) {
+            itemIndex = count + itemIndex % count;
+        }
+        return itemIndex;
+    }
+
+    protected final void resetAllItem() {
+        noNeedLayout = true;
+        for (ItemWrapper item : items) {
+            item.recycle();
+        }
+        noNeedLayout = false;
+        currentItemIndex = 0;
+    }
+
+    private final class AdapterDataSetObserver extends DataSetObserver {
+        @Override
+        public void onChanged() {
+            resetAllItem();
+            requestLayout();
+        }
+
+        @Override
+        public void onInvalidated() {
+            resetAllItem();
+            requestLayout();
+        }
+    }
+
+    /**
+     * Wrapper (contains data id and status)
+     */
+    private final class ItemWrapper {
+
+        public static final int NONE = 0x0;
+        public static final int USEING = 0x1;
+        public static final int FORCE = 0x2;
+
+        private int status;
+        private int dataIndex;
+        private View view;
+
+        public ItemWrapper() {
+            this.status = NONE;
+            this.dataIndex = 0;
+            this.view = null;
+        }
+
+        public ItemWrapper(int index, View v) {
+            this.dataIndex = index;
+            this.view = v;
+            this.status = NONE;
+        }
+
+        public void recycle() {
+            setView(null);
+            setDataIndex(0);
+            setStatus(NONE);
+        }
+
+        public View getView() {
+            return view;
+        }
+
+        public void setView(View v) {
+            if (v == null && view != null) {
+                ((ViewGroup) view.getParent()).removeView(view);
+            }
+            this.view = v;
+        }
+
+        public void setDataIndex(int dataIndex) {
+            this.dataIndex = dataIndex;
+            this.status = USEING;
+        }
+
+        public int getDataIndex() {
+            return dataIndex;
+        }
+
+        public int getStatus() {
+            return status;
+        }
+
+        public void setStatus(int status) {
+            this.status = status;
+        }
+
+    }
+
+}
--- demo/src/main/java/org/pinwheel/demo4agility/activity/CycleGalleryActivity.java
+++ demo/src/main/java/org/pinwheel/demo4agility/activity/CycleGalleryActivity.java
@@ -1,46 +1,49 @@
 package org.pinwheel.demo4agility.activity;
 
+import android.graphics.Color;
 import android.os.Handler;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.*;
 import org.pinwheel.agility.adapter.SimpleArrayAdapter;
-import org.pinwheel.agility.view.SweetCycleGallery;
+import org.pinwheel.agility.view.SweetCircularView;
 
 import java.util.Arrays;
 
 
 public class CycleGalleryActivity extends AbsTestActivity {
 
-    private SweetCycleGallery gallery;
+    private SweetCircularView gallery;
 
     private SimpleArrayAdapter adapter = new SimpleArrayAdapter<Integer>() {
         @Override
         public View getView(final int position, View convertView, ViewGroup parent) {
-            convertView = createView(position);
 //            if (convertView == null) {
-//                convertView = new Button(CycleGalleryActivity.this);
+//            convertView = createView(position);
 //            }
-//
-//            Button btn = (Button) convertView;
-//            btn.setGravity(Gravity.LEFT | Gravity.TOP);
-//            btn.setTextSize(32);
-//            btn.setText("" + position);
-//            btn.setBackgroundColor(getResources().getColor(getItem(position)));
+            if (convertView == null) {
+                convertView = new Button(CycleGalleryActivity.this);
+            }
+
+            Button btn = (Button) convertView;
+            btn.setGravity(Gravity.CENTER);
+            btn.setTextSize(32);
+            btn.setText("" + position);
+            btn.setBackgroundColor(getResources().getColor(getItem(position)));
 //            convertView.setOnClickListener(new View.OnClickListener() {
 //                @Override
 //                public void onClick(View v) {
-//                    Toast.makeText(CycleGalleryActivity.this, "" + position, Toast.LENGTH_SHORT).show();
+//                    logout(position);
 //                }
 //            });
-            Log.e("-----", "currentIndex:" + gallery.getCurrentItem());
+            logout("getView() position:" + position);
             return convertView;
         }
 
         private View createView(final int position) {
             HorizontalScrollView scrollView = new HorizontalScrollView(CycleGalleryActivity.this);
+            scrollView.setBackgroundColor(Color.WHITE);
             LinearLayout linearLayout = new LinearLayout(CycleGalleryActivity.this);
             linearLayout.setOrientation(LinearLayout.HORIZONTAL);
             TextView textView = new TextView(CycleGalleryActivity.this);
@@ -49,7 +52,7 @@ public class CycleGalleryActivity extends AbsTestActivity {
             textView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Toast.makeText(CycleGalleryActivity.this, "" + position, Toast.LENGTH_SHORT).show();
+                    logout(position);
                 }
             });
             LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(1500, -1);
@@ -62,91 +65,82 @@ public class CycleGalleryActivity extends AbsTestActivity {
 
     @Override
     protected void onInitInCreate() {
-//        adapter.addAll(Arrays.asList(
+        adapter.addAll(Arrays.asList(
 //                android.R.color.holo_red_dark,
 //                android.R.color.holo_orange_dark,
 //                android.R.color.holo_blue_dark,
 //                android.R.color.holo_green_dark,
 //                android.R.color.darker_gray
-//        ));
+        ));
     }
+
+    Handler handler = new Handler();
 
     @Override
     protected View getContentView() {
-//        new Handler(getMainLooper()).postDelayed(new Runnable() {
-//            @Override
-//            public void run() {
-//                gallery.setAdapter(adapter);
-//            }
-//        }, 3000l);
-        new Handler(getMainLooper()).postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                adapter.addAll(Arrays.asList(
-                        android.R.color.holo_red_dark,
-                        android.R.color.holo_orange_dark,
-                        android.R.color.holo_blue_dark,
-                        android.R.color.holo_green_dark,
-                        android.R.color.darker_gray
-                ));
-                adapter.notifyDataSetChanged();
-            }
-        }, 3000l);
-
         FrameLayout container = new FrameLayout(this);
 
-        gallery = new SweetCycleGallery(this);
+        gallery = new SweetCircularView(this);
         gallery.setAdapter(adapter);
-        gallery.setOrientation(SweetCycleGallery.VERTICAL);
-        gallery.setAutoScrollInertial(0.5f);
+        gallery.setOrientation(LinearLayout.HORIZONTAL);
+        gallery.setSensibility(0.25f);
         FrameLayout.LayoutParams galleryParams = new FrameLayout.LayoutParams(-1, -1);
-        gallery.setPadding(50, 50, 50, 50);
-        gallery.setBackgroundColor(getResources().getColor(android.R.color.white));
         galleryParams.setMargins(50, 50, 50, 50);
         container.addView(gallery, galleryParams);
 
         FrameLayout.LayoutParams left = new FrameLayout.LayoutParams(-2, -2, Gravity.LEFT | Gravity.CENTER_VERTICAL);
         Button leftBtn = new Button(this);
-        leftBtn.setText("<");
+        leftBtn.setText("Fun0");
         container.addView(leftBtn, left);
 
         FrameLayout.LayoutParams right = new FrameLayout.LayoutParams(-2, -2, Gravity.RIGHT | Gravity.CENTER_VERTICAL);
         Button rightBtn = new Button(this);
-        rightBtn.setText(">");
+        rightBtn.setText("Fun1");
         container.addView(rightBtn, right);
 
         FrameLayout.LayoutParams top = new FrameLayout.LayoutParams(-2, -2, Gravity.TOP | Gravity.CENTER_HORIZONTAL);
         Button topBtn = new Button(this);
-        topBtn.setText("^");
+        topBtn.setText("Fun2");
         container.addView(topBtn, top);
 
         FrameLayout.LayoutParams bottom = new FrameLayout.LayoutParams(-2, -2, Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL);
         Button bottomBtn = new Button(this);
-        bottomBtn.setText("v");
+        bottomBtn.setText("Fun3");
         container.addView(bottomBtn, bottom);
 
         leftBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                gallery.moveLeft();
-            }
-        });
-        rightBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                gallery.moveRight();
+//                gallery.setCurrentIndex(gallery.getCurrentIndex() - 1);
+                adapter.addItem(android.R.color.holo_red_dark);
+                adapter.addItem(android.R.color.holo_orange_dark);
+                adapter.addItem(android.R.color.holo_blue_dark);
+                adapter.addItem(android.R.color.holo_green_dark);
+                adapter.notifyDataSetChanged();
             }
         });
         topBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                gallery.moveTop();
+//                adapter.removeAll();
+//                adapter.notifyDataSetChanged();
+                gallery.setCurrentIndex(gallery.getCurrentIndex() - 1);
+            }
+        });
+        rightBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                adapter.addItem(android.R.color.darker_gray);
+                adapter.notifyDataSetChanged();
             }
         });
         bottomBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                gallery.moveBottom();
+//                adapter.removeAll();
+//                adapter.addItem(android.R.color.holo_blue_dark);
+//                adapter.notifyDataSetChanged();
+                gallery.setCurrentIndex(gallery.getCurrentIndex() + 1);
             }
         });
 
